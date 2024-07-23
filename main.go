fece4129e0d17a2d028c67ad4bfcdcce109681ca
Soozism <a.soozism@gmail.com>
2024-07-23 13:43:30 +0330

    fun

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	ns := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for _, s := range ns {
-		if s%2 == 0 {
-			fmt.Printf("%v is even \n", s)
+	for _, n := range ns {
+		if n%2 == 0 {
+			fmt.Printf("%v is even \n", n)
 		}else{
-			fmt.Printf("%v is odd \n", s)
+			fmt.Printf("%v is odd \n", n)
 		}
 	}
 
